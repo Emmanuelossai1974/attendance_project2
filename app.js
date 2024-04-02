c741576e3e034b1555297b87e9532ef815012b7d
Emmanuelossai1974 <chukwudi.ossai@yahoo.com>
2024-04-02 11:50:46 -0600

    My Update

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 
 require('dotenv').config;
-
+const mongoose = require(`mongoose`);
 const app = express();
 
 
+app.set(`view engine`, `ejs`);
+app.set(`view engine`, `/views`);
+
+app.use(express.static(`public`));
+
+const url="mongodb+srv://chukwudiossai1974:Chukwudi1@cluster0.tt8vgje.mongodb.net/";
 
 
 
+
+mongoose.connect(url)
+.then(() =>{
+  console.log(`Connected to MongoDB Database`);
+})
+.catch((err) =>{
+  console.log(`Error connecting to the database: ${err}`)
+})
 
 
 
