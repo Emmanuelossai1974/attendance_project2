22437af8f7662e4f0274414b82b3191a18c70837
Emmanuelossai1974 <chukwudi.ossai@yahoo.com>
2024-04-08 11:45:52 -0600

    my commit

--- app.js
+++ app.js
@@ -1,33 +1,114 @@
 const express = require('express');
-
 require('dotenv').config;
-const mongoose = require(`mongoose`);
-const jwt = require(`jsonwebtoken`);
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const Student = require('./student.js');
 const app = express();
 
 
-app.set(`view engine`, `ejs`);
-app.set(`views`, `./views`);
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-app.use(express.static(`public`));
+//Middleware (use)
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
-const url="mongodb+srv://chukwudiossai1974:Chukwudi1@cluster0.tt8vgje.mongodb.net/";
+const url="mongodb+srv://chukwudiossai1974:password321@cluster0.tt8vgje.mongodb.net/";
 
 
-app.get(`/`, (req, res) =>{
-  res.render((`login`));
+app.get('/', (req, res) =>{
+  res.render('login');
 });
+
+app.get('/home', (req, res) => {
+  res.render('attendance');
+})
+
+
+app.post('/' , async (req, res) => {
+
+  const email = req.body.email;
+  const password = req.body.password;
+  const secretKey = 'my_secret_key';
+
+  //Find user in the database by email
+  const user =  await Student.findOne({email});
+
+  if(!user){
+    //User not found
+    res.status(404).send('User not found');
+    return;
+  }
+
+  //Creating and signing a JWT
+  const unique = user._id.toString();
+
+  //Create a jwt
+  const token = jwt.sign(unique, secretKey);
+  
+  //Stuff the token (jwt) inside the cookie and issue it
+   res.cookie('jwt', token, {maxAge: 5 * 60 * 1000, httpOnly: true});
+
+   bcrypt.compare(password, user.password, (err, result) =>{
+
+    if(result){
+        res.redirect('home');
+    } else {
+      res.send('Password does not match our records. Please try again')
+    }
+  });
+});
+
+
+app.post('/register', (req, res) =>{
+
+  const {email, password, confirmPassword} = req.body;
+
+  const user = Student.findOne({email});
+
+  //Check if username already exists.  
+  //if(user){
+    //  res.status(400).send('Username already exists.  Please try again');
+     // return;
+  //}
+
+  //Check if the confirm password equals the password
+  if(password !== confirmPassword){
+        res.status(400).send('Passwords do not match!');
+        return;
+  }
+
+  bcrypt.hash(password, 12, (err, hashedPassword) =>{
+
+    const user = new Student({
+      email: email,
+      password: hashedPassword,
+    });
+
+    user.save();
+
+    res.redirect('/');
+
+  });
+});
+
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
 
 mongoose.connect(url)
 .then(() =>{
-  console.log(`Connected to MongoDB Database`);
+  console.log('Connected to MongoDB Database');
 })
-.catch((err) =>{
+.catch((err)=>{
   console.log(`Error connecting to the database: ${err}`)
-})
-
-
-
+});
 
 
 
@@ -35,10 +116,6 @@ mongoose.connect(url)
 
 const PORT = process.env.PORT || 3000;
 
-
-
 app.listen(PORT, ()=>{
-
   console.log(`Successfully connected to ${PORT}`);
-
 });
